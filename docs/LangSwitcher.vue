bc6fa0e655a7510e7c898d3e238820a4263a60e8
Frizi <frizi09@gmail.com>
2016-11-16 13:47:44 +0100

    template language switcher

---
+++ docs/LangSwitcher.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+  div.switcher
+    button.switchbutton(
+      v-for="lang in langs"
+      v-bind:class="{'selected': lang === current}"
+      @click="select(lang)"
+    ) {{lang}}
+</template>
+
+<script>
+export default {
+  props: ['langs', 'current'],
+  methods: {
+    select (lang) {
+      this.$emit('select', lang)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .switcher {
+    display: inline-block;
+    border-radius: 2px;
+    overflow:hidden;
+  }
+
+  .switchbutton {
+    border: none;
+    background-color: #EAEAEA;
+    color: #848484;
+    padding: 1px 10px;
+    text-transform: capitalize;
+  }
+
+  .switcher > * + * {
+    border-left: 1px solid #CCC;
+  }
+
+  .switchbutton:focus {
+    outline: none;
+    box-shadow: inset 0 0 4px -1px black;
+  }
+
+  .switchbutton.selected {
+    background-color: #CFCFCF;
+    color: #707070;
+  }
+</style>
--- docs/main.js
+++ docs/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Validation from '../src/index'
 
 import * as examples from './partials/examples'
+import LangSwitcher from './LangSwitcher'
 
 Vue.use(Validation)
 
@@ -26,13 +27,16 @@ const SL = ', 100%, 85%'
 new Vue({
   el: '#app',
   components: {
+    LangSwitcher,
     ...examples
   },
   data () {
     return {
       isNavSticky: false,
       firstColor: Math.floor(Math.random() * 255),
-      secondColor: Math.floor(Math.random() * 255)
+      secondColor: Math.floor(Math.random() * 255),
+      markupLangs: ['pug', 'html'],
+      markupLanguage: 'pug'
     }
   },
   computed: {
@@ -43,6 +47,9 @@ new Vue({
     }
   },
   methods: {
+    selectLanguage (lang) {
+      this.markupLanguage = lang
+    },
     adjustNav () {
       this.isNavSticky = window.scrollY > window.innerHeight
     }
