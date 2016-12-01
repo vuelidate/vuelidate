8393984ef4ba80cf0e6bd8c0b31758b1daf0ab43
Alex Kyriakidis <hootlex@icloud.com>
2016-12-01 21:41:36 +0200

    - improve regex - allow empty strings

--- test/unit/specs/validators/email.spec.js
+++ test/unit/specs/validators/email.spec.js
@@ -1,8 +1,8 @@
 import email from 'src/validators/email'
 
 describe('email validator', () => {
-  it('should not validate empty string', () => {
-    expect(email('')).to.be.false
+  it('should validate empty string', () => {
+    expect(email('')).to.be.true
   })
 
   it('should not validate numbers', () => {
@@ -28,7 +28,6 @@ describe('email validator', () => {
 
   it('should not validate addresses that contain unsupported characters', () => {
     expect(email('someone@g~mail.com')).to.be.false
-    expect(email('some!one@gmail.com')).to.be.false
     expect(email('someone@g=ail.com')).to.be.false
   })
 
@@ -41,8 +40,10 @@ describe('email validator', () => {
   it('should validate real addresses', () => {
     expect(email('someone@gmail.com')).to.be.true
     expect(email('someone@g-mail.com')).to.be.true
+    expect(email('some!one@gmail.com')).to.be.true
     expect(email('soMe12_one@gmail.com')).to.be.true
     expect(email('someone@gmail.co')).to.be.true
     expect(email('someone@g.cn')).to.be.true
+    expect(email('someone@g.accountants')).to.be.true
   })
 })
