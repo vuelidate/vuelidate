f4609250f6d619e55a6061aa955e8512de9da58d
Ecmel Ercan <ecmel.ercan@gmail.com>
2017-02-11 13:36:25 +0200

    Fix email and sameAs validators for undefined, null and empty values

--- src/validators/email.js
+++ src/validators/email.js
@@ -1,4 +1,7 @@
 export default value => {
+  if (typeof value === 'undefined' || value === null || value === '') {
+    return true
+  }
   let reg = /(^$|^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$)/
   return reg.test(value)
 }
--- src/validators/sameAs.js
+++ src/validators/sameAs.js
@@ -1,8 +1,4 @@
 export default equalTo => function (value, parentVm) {
-  if (value === null || value === undefined || value === '') {
-    return true
-  }
-
   const compareTo = typeof equalTo === 'function'
     ? equalTo.call(this, parentVm)
     : parentVm[equalTo]
