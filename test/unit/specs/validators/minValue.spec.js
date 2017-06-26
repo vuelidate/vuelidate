cbd4774911d38e63017cbb54b4e820808fbe2360
Felipe Bohnert Paetzold <felipe@paliari.com.br>
2017-06-26 13:44:17 -0300

    add unit-tests

---
+++ test/unit/specs/validators/minValue.spec.js
@@ -0,0 +1,20 @@
+import minValue from 'src/validators/minValue'
+
+describe('minValue validator', () => {
+  it('should ', () => {
+    expect(minValue(5)(5)).to.be.true
+  })
+
+  it('should validate the valid number', () => {
+    expect(minValue(5)(6)).to.be.true
+  })
+
+  it('should validate the invalid number', () => {
+    expect(minValue(5)(4)).to.be.false
+  })
+
+  it('should validate the string value', () => {
+    expect(minValue(5)('not string here')).to.be.false
+  })
+
+})
