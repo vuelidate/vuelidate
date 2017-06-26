2f8670408858154e61c175c9214e143f0ea1779d
Felipe Bohnert Paetzold <felipe@paliari.com.br>
2017-06-26 11:54:01 -0300

    remove from examples

--- docs/partials/examples/ExampleBasic.vue
+++ docs/partials/examples/ExampleBasic.vue
@@ -14,34 +14,16 @@
       input.form__input(v-model.trim="age" @blur="$v.age.$touch()")
     span.form-group__message(v-if="!$v.age.between")
       | Must be between {{$v.age.$params.between.min}} and {{$v.age.$params.between.max}}
-    pre
-      | age: {{ $v.age }}
-    .form-group(v-bind:class="{ 'form-group--error': $v.cookies.$error }")
-      label.form__label Cookies
-      input.form__input(v-model.trim="cookies" @blur="$v.cookies.$touch()")
-    span.form-group__message(v-if='!$v.cookies.minValue')
-      | Must have minimum {{ $v.cookies.$params.minValue.min }}
-    pre
-      | cookies: {{ $v.cookies }}
-    .form-group(v-bind:class="{ 'form-group--error': $v.books.$error }")
-      label.form__label Books
-      input.form__input(v-model.trim="books" @blur="$v.books.$touch()")
-    span.form-group__message(v-if='!$v.books.maxValue')
-      | Must have maximum {{ $v.books.$params.maxValue.max }}
-    pre
-      | books: {{ $v.books }}
 </template>
 
 <script>
-import { required, minLength, between, minValue, maxValue } from 'vuelidate/lib/validators'
+import { required, minLength, between } from 'vuelidate/lib/validators'
 
 export default {
   data () {
     return {
       name: '',
-      age: 0,
-      cookies: 0,
-      books: 2
+      age: 0
     }
   },
   validations: {
@@ -51,12 +33,6 @@ export default {
     },
     age: {
       between: between(20, 30)
-    },
-    cookies: {
-      minValue: minValue(1)
-    },
-    books: {
-      maxValue: maxValue(5)
     }
   }
 }
