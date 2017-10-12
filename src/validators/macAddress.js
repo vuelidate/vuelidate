e44a1311bd154916e147126a959c4e9cc2d1368f
Ashley Hood <ashley@opensauce.systems>
2017-10-12 11:59:42 +0100

    add match for EUI-48/MAC-48 addresses

--- src/validators/macAddress.js
+++ src/validators/macAddress.js
@@ -13,7 +13,7 @@ export default (separator = ':') => withParams(
       ? value.split(separator)
       : value.length === 16 ? value.match(/.{2}/g) : null
 
-    return parts !== null && parts.length === 8 && parts.every(hexValid)
+    return parts !== null && (parts.length === 6 || parts.length === 8) && parts.every(hexValid)
   })
 
 const hexValid = (hex) => hex.toLowerCase().match(/^[0-9a-f]{2}$/)
