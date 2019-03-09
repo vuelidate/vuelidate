5d57326e77f7b2c5a787ccd572d60da28d5fbd93
Ben Hong <bencodezen@gmail.com>
2019-03-09 11:09:23 -0500

    feature: Add logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,5 +8,8 @@ module.exports = {
     [ 'link', { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" }],
     [ 'meta', { name: "msapplication-TileColor", content: "#da532c" }],
     [ 'meta', { name: "theme-color", content: "#ffffff" }]
-  ]
+  ],
+  themeConfig: {
+    logo: 'logo.png'
+  }
 }
