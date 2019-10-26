7499efa4615361e7cfd2d42d6490ccd3738dc71d
dobromir-hristov <dobromir92@gmail.com>
2019-10-26 19:30:23 +0300

    feat (*): add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    // 'plugin:vue/essential', // is this required?
+    'standard'
+  ],
+  globals: {},
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [],
+  rules: {}
+}
