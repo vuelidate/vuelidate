aab11d16a804550f7c3bc3acebd2af0f850486ca
dobromir-hristov <dobromir92@gmail.com>
2020-01-03 20:36:52 +0200

    feat(validators): add promise as property resolver
    Works with requiredIf and requiredUnless

--- packages/validators/src/raw/__tests__/requiredIf.spec.js
+++ packages/validators/src/raw/__tests__/requiredIf.spec.js
@@ -2,6 +2,8 @@ import requiredIf from '../requiredIf'
 
 const T = () => true
 const F = () => false
+const promiseT = () => Promise.resolve(true)
+const promiseF = () => Promise.resolve(false)
 
 describe('requiredIf validator', () => {
   it('should not validate empty string when functional condition is met', () => {
@@ -18,5 +20,19 @@ describe('requiredIf validator', () => {
 
   it('should validate empty string when simple boolean condition not met', () => {
     expect(requiredIf('')('')).toBe(true)
+  })
+
+  it('should return a promise when passed a promise condition', () => {
+    const response = requiredIf(promiseT)('')
+    expect(response).toHaveProperty('then') // is a promise
+  })
+
+  it('should validate value if condition is a truthy promise', async () => {
+    expect(await requiredIf(promiseT)('')).toBe(false)
+    expect(await requiredIf(promiseT)('someValue')).toBe(true)
+  })
+
+  it('should NOT validate value if condition is a falsy promise', async () => {
+    expect(await requiredIf(promiseF)('')).toBe(true)
   })
 })
