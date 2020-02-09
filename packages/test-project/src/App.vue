0f2db5e63feab16c16d8edee3f3690e0a900ab4f
dobromir-hristov <dobromir92@gmail.com>
2020-02-09 23:27:05 +0200

    feat(test-project): add a test project in Vue 3

---
+++ packages/test-project/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <nested-validations />
+  </div>
+</template>
+
+<script>
+import NestedValidations from '@/components/NestedValidations'
+
+export default {
+  name: 'App',
+  components: {
+    NestedValidations
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ packages/test-project/src/components/NestedValidations.vue
@@ -0,0 +1,37 @@
+<template>
+  <div style="padding-top: 2rem;">
+    <div style="margin-bottom: 20px">
+      <label>Number X</label>
+      <input
+        v-model.number="numberX"
+        type="number"
+      >
+    </div>
+    <pre style="background-color: white;">
+      {{ vuelidate }}
+    </pre>
+  </div>
+</template>
+
+<script>
+import { ref, reactive } from 'vue'
+import useVuelidate from '@vuelidate/core/src'
+import { required, minValue } from '@vuelidate/validators/src/withMessages'
+
+export default {
+  setup () {
+    const numberX = ref(0)
+
+    const state = reactive({
+      b: 'b'
+    })
+
+    let vuelidate = useVuelidate(
+      { numberX: { required, minValue: minValue(3) } },
+      { numberX }
+    )
+
+    return { vuelidate, numberX, state }
+  }
+}
+</script>
