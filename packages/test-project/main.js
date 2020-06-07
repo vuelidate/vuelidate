d08f8c74f9a7dc8bc6c2f0dcc3ea98d3b5702e98
dobromir-hristov <dobromir92@gmail.com>
2020-06-07 09:44:02 +0300

    feat: add vue-router

--- packages/test-project/main.js
+++ packages/test-project/main.js
@@ -1,4 +1,31 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './src/App.vue'
+import { routes } from './src/routes.js'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+let router = createRouter({
+  history: createWebHistory(),
+  routes: __DEV__ ? [] : routes,
+})
+
+if (import.meta.hot) {
+  let removeRoutes = []
+
+  for (let route of routes) {
+    removeRoutes.push(router.addRoute(route))
+  }
+
+  import.meta.hot.acceptDeps('./routes.js', ({ routes }) => {
+    for (let removeRoute of removeRoutes) removeRoute()
+    removeRoutes = []
+    for (let route of routes) {
+      removeRoutes.push(router.addRoute(route))
+    }
+    router.replace('')
+  })
+}
+
+app.use(router)
+app.mount('#app')
