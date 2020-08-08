fa8016fd5d0b5ad7ca3dc8a50104c9ba7c7a3732
shentao <damian.dulisz@gmail.com>
2020-08-08 14:59:10 +0200

    chore: update jest setup
    Co-Authored-By: Dobromir Hristov <dobromir-hristov@users.noreply.github.com>

--- packages/vuelidate/jest.config.js
+++ packages/vuelidate/jest.config.js
@@ -3,5 +3,7 @@ const base = require('../../jest.base')
 module.exports = {
   ...base,
   name: 'Vuelidate',
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.js'],
   displayName: 'Vuelidate'
 }
