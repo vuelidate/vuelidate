ee11bb120fd4e7ba333a75b55cfda58cb0575b83
shentao <damian.dulisz@gmail.com>
2020-09-13 15:49:44 +0200

    chore(compat): remove old compat version

--- packages/vuelidate-compat/bili.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('bili').Config} */
-module.exports = {
-  input: 'src/index.js',
-  output: {
-    format: ['es', 'cjs'],
-    moduleName: 'Vuelidate'
-  }
-}
--- packages/vuelidate-compat/jest.config.js
+++
@@ -1,9 +0,0 @@
-const base = require('../../jest.base')
-
-module.exports = {
-  ...base,
-  name: 'Vuelidate',
-  collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.js'],
-  displayName: 'Vuelidate'
-}
