06d5e49154897bce109d576dd70739b659d2d9dc
shentao <damian.dulisz@gmail.com>
2020-09-13 15:28:39 +0200

    fix(compat): fix mixin; fix plugin object

--- packages/vuelidate/src/index.js
+++ packages/vuelidate/src/index.js
@@ -68,12 +68,12 @@ export function useVuelidate (validations, state, registerAs) {
  */
 
 export const VuelidateMixin = {
+  computed: {},
   beforeCreate () {
     const resultsCache = new Map()
     const options = this.$options
     if (!options.validations) return
 
-    if (!options.computed) options.computed = {}
     if (options.computed.$v) return
 
     const validations = computed(() => isFunction(options.validations)
@@ -97,8 +97,10 @@ export const VuelidateMixin = {
  * Default way to install Vuelidate globally for entire app.
  * @param {Vue} app
  */
-export function VuelidatePlugin (app) {
-  app.mixin(VuelidateMixin)
+export const VuelidatePlugin = {
+  install (app) {
+    app.mixin(VuelidateMixin)
+  }
 }
 
 export default useVuelidate
