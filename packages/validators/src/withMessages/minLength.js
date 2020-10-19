38490e2a3825a6b3f4e9332b1f3b331b3cd0a4c5
Ivan Bozhkov <bozhkov.code@gmail.com>
2020-10-19 17:46:57 +0200

    Unify minLength validator format

--- packages/validators/src/withMessages/minLength.js
+++ packages/validators/src/withMessages/minLength.js
@@ -1,7 +1,7 @@
 import minLength from '../raw/minLength'
 
-export default length => ({
-  $validator: minLength(length),
-  $message: ({ $params }) => `This field should be at least ${$params.length} long.`,
-  $params: { length }
+export default (min) => ({
+  $validator: minLength(min),
+  $message: ({ $params }) => `This field should be at least ${$params.min} long.`,
+  $params: { min }
 })
