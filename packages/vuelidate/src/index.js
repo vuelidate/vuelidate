1ae1930b123a792614ef04ebc58f4c188e98bb13
shentao <damian.dulisz@gmail.com>
2020-10-24 21:35:18 +0200

    refactor(core): unlazy the validations; add $silentErrors

--- packages/vuelidate/src/index.js
+++ packages/vuelidate/src/index.js
@@ -1,4 +1,4 @@
-import { reactive, toRefs, provide, inject, ref, computed, getCurrentInstance, onBeforeUnmount } from 'vue-demi'
+import { reactive, provide, inject, ref, computed, getCurrentInstance, onBeforeUnmount } from 'vue-demi'
 import { unwrap, isFunction } from './utils'
 import { setValidations } from './core'
 
@@ -10,31 +10,37 @@ const VuelidateRemoveChildResults = Symbol('vuelidate#removeChiildResults')
  * Use inside the `setup` lifecycle hook
  * @param {Object} validations - Validations Object
  * @param {Object} state - State object
- * @param {String} registerAs - a registration name, when registering results to the parent validator.
+ * @param {String} globalConfig - Config Object
  * @return {UnwrapRef<*>}
  */
-export function useVuelidate (validations, state, registerAs) {
+export function useVuelidate (validations, state, globalConfig = {}) {
+  const isOptionsApiMode = !validations
+
   if (!validations) {
     const instance = getCurrentInstance()
     if (instance.type.validations) {
       const rules = instance.type.validations
 
-      state = computed(() => toRefs(reactive(instance.ctx)))
+      state = computed(() => reactive(instance.ctx))
       validations = computed(() => isFunction(rules)
         ? rules.call(state.value)
         : rules
       )
+
+      globalConfig = instance.type.validationsConfig || {}
     }
   }
 
+  let { $registerAs } = globalConfig
+
   // if there is no registration name, add one.
-  if (!registerAs) {
+  if (!$registerAs) {
     const instance = getCurrentInstance()
     // NOTE:
     // ._uid // Vue 2.x Composition-API plugin
     // .uid // Vue 3.0
     const uid = instance.uid || instance._uid
-    registerAs = `_vuelidate_${uid}`
+    $registerAs = `_vuelidate_${uid}`
   }
 
   const resultsCache = new Map()
@@ -75,22 +81,34 @@ export function useVuelidate (validations, state, registerAs) {
   // provide to all of it's children the remove results  function
   provide(VuelidateRemoveChildResults, removeChildResultsFromParent)
 
-  const validationResults = computed(() => setValidations({
-    validations: unwrap(validations),
-    state,
-    childResults,
-    resultsCache
-  }))
+  // TODO: This should likely be refactored at some point once we figure out Options API
+  // limitations. Otherwise it might lead to memory leaks.
+  const validationResults = isOptionsApiMode
+    ? computed(() => setValidations({
+      validations,
+      state,
+      childResults,
+      resultsCache,
+      globalConfig
+    }))
+    : setValidations({
+      validations,
+      state,
+      childResults,
+      resultsCache,
+      globalConfig
+    })
 
   // send all the data to the parent when the function is invoked inside setup.
-  sendValidationResultsToParent(validationResults, registerAs)
+  sendValidationResultsToParent(validationResults, $registerAs)
   // before this component is destroyed, remove all the data from the parent.
-  onBeforeUnmount(() => removeValidationResultsFromParent(registerAs))
+  onBeforeUnmount(() => removeValidationResultsFromParent($registerAs))
 
   // TODO: Change into reactive + watch
   return computed(() => {
+    const results = isOptionsApiMode ? validationResults.value : validationResults
     return {
-      ...validationResults.value,
+      ...results,
       ...childResults.value
     }
   })
