a1bcf5d6aa048df8d4eaaa31561521af60c12d17
shentao <damian.dulisz@gmail.com>
2020-10-25 02:09:48 +0200

    chore(tests): adjust tests after unlazyfying the validations

--- packages/vuelidate/test/unit/utils.js
+++ packages/vuelidate/test/unit/utils.js
@@ -18,9 +18,10 @@ export const createSimpleComponent = (getVuelidateResults, state) => ({
   }
 })
 
-export const createSimpleWrapper = (rules, state) => mount(createSimpleComponent(() => useVuelidate(rules, state), state))
+export const createSimpleWrapper = (rules, state, config = {}) => mount(createSimpleComponent(() => useVuelidate(rules, state, config), state))
 
 export const shouldBePristineValidationObj = (v) => {
+  expect(v).toHaveProperty('$error', false)
   expect(v).toHaveProperty('$error', false)
   expect(v).toHaveProperty('$errors', [])
   expect(v).toHaveProperty('$silentErrors', [])
@@ -33,8 +34,35 @@ export const shouldBePristineValidationObj = (v) => {
 }
 
 export const shouldBeInvalidValidationObject = ({ v, property, propertyPath = property, validatorName }) => {
+  expect(v).toHaveProperty('$error', false)
+  expect(v).toHaveProperty('$errors', [])
+  expect(v).toHaveProperty('$silentErrors', [{
+    $message: '',
+    $params: {},
+    $pending: false,
+    $property: property,
+    $propertyPath: propertyPath,
+    $validator: validatorName
+  }])
+  expect(v).toHaveProperty('$invalid', true)
+  expect(v).toHaveProperty('$pending', false)
+  expect(v).toHaveProperty('$dirty', false)
+  expect(v).toHaveProperty('$anyDirty', false)
+  expect(v).toHaveProperty('$touch', expect.any(Function))
+  expect(v).toHaveProperty('$reset', expect.any(Function))
+}
+
+export const shouldBeErroredValidationObject = ({ v, property, propertyPath = property, validatorName }) => {
   expect(v).toHaveProperty('$error', true)
   expect(v).toHaveProperty('$errors', [{
+    $message: '',
+    $params: {},
+    $pending: false,
+    $property: property,
+    $propertyPath: propertyPath,
+    $validator: validatorName
+  }])
+  expect(v).toHaveProperty('$silentErrors', [{
     $message: '',
     $params: {},
     $pending: false,
