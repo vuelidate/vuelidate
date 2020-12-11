e77a61039afcac3dcce66d84d4229ead5b050a58
helmi77 <helmi77@outlook.com>
2020-12-11 23:00:28 +0100

    fix: remove side effects from withMessage helper (#763)

--- packages/validators/src/utils/withMessage.js
+++ packages/validators/src/utils/withMessage.js
@@ -16,7 +16,7 @@ export default function withMessage ($message, $validator) {
   if (!isFunction($message) && typeof unwrap($message) !== 'string') throw new Error(`[@vuelidate/validators]: First parameter to "withMessage" should be string or a function returning a string, provided ${typeof $message}`)
   if (!isObject($validator) && !isFunction($validator)) throw new Error(`[@vuelidate/validators]: Validator must be a function or object with $validator parameter`)
 
-  const validatorObj = normalizeValidatorObject($validator)
+  const validatorObj = Object.assign({}, normalizeValidatorObject($validator))
   validatorObj.$message = $message
 
   return validatorObj
