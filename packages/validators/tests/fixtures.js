83a974fb978946e5685ae19b23cd0038418a3d7b
dobromir-hristov <dobromir92@gmail.com>
2021-03-18 00:59:32 +0200

    chore: fix test fixtures for rich validator response

--- packages/validators/tests/fixtures.js
+++ packages/validators/tests/fixtures.js
@@ -1,7 +1,7 @@
 export const T = () => true
 export const F = () => false
-export const ValidatorResponseT = () => ({ $invalid: true })
-export const ValidatorResponseF = () => ({ $invalid: false })
+export const ValidatorResponseT = () => ({ $valid: true })
+export const ValidatorResponseF = () => ({ $valid: false })
 export const NormalizedT = { $validator: T }
 export const NormalizedF = { $validator: F }
 export const NormalizedValidatorResponseT = { $validator: ValidatorResponseT }
