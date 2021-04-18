93f57a6f7eabe1cf25b3d587c9286cfa215bac9b
Dobromir Hristov <dobromir92@gmail.com>
2021-04-18 20:55:23 +0300

    fix(validators): properly pass the this context to validators (#845)

--- packages/validators/src/raw/__tests__/requiredIf.spec.js
+++ packages/validators/src/raw/__tests__/requiredIf.spec.js
@@ -5,25 +5,25 @@ const promiseT = () => Promise.resolve(true)
 const promiseF = () => Promise.resolve(false)
 
 describe('requiredIf validator', () => {
-  it('should not validate empty string when functional condition is met', () => {
-    expect(requiredIf(T)('')).resolves.toBe(false)
+  it('should not validate empty string when functional condition is met', async () => {
+    await expect(requiredIf(T)('')).resolves.toBe(false)
   })
 
-  it('should validate empty string when functional condition not met', () => {
-    expect(requiredIf(F)('')).resolves.toBe(true)
+  it('should validate empty string when functional condition not met', async () => {
+    await expect(requiredIf(F)('')).resolves.toBe(true)
   })
 
-  it('should not validate empty string when simple boolean condition is met', () => {
-    expect(requiredIf('prop')('')).resolves.toBe(false)
+  it('should not validate empty string when simple boolean condition is met', async () => {
+    await expect(requiredIf('prop')('')).resolves.toBe(false)
   })
 
-  it('should validate empty string when simple boolean condition not met', () => {
-    expect(requiredIf('')('')).resolves.toBe(true)
+  it('should validate empty string when simple boolean condition not met', async () => {
+    await expect(requiredIf('')('')).resolves.toBe(true)
   })
 
-  it('should return a promise when passed a promise condition', () => {
+  it('should return a promise when passed a promise condition', async () => {
     const response = requiredIf(promiseT)('')
-    expect(response).toHaveProperty('then') // is a promise
+    await expect(response).toHaveProperty('then') // is a promise
   })
 
   it('should validate value if condition is a truthy promise', async () => {
@@ -40,5 +40,13 @@ describe('requiredIf validator', () => {
     requiredIf(validator)('foo', 'bar')
     expect(validator).toHaveBeenCalledTimes(1)
     expect(validator).toHaveBeenCalledWith('foo', 'bar')
+  })
+
+  it('should have the correct `this` context', async () => {
+    const validator = jest.fn(function () { return this.foo === 'foo' })
+    const context = { foo: 'foo' }
+    const result = await requiredIf.call(context, validator)('value', 'parentVM')
+    expect(validator).toHaveReturnedWith(true)
+    expect(result).toEqual(true)
   })
 })
