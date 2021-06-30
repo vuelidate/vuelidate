ca6bb321e7a6d69d108aec12e786994737987634
Dobromir Hristov <dobromir92@gmail.com>
2021-06-30 08:58:50 +0300

    feature(core): pass more parameters to `$message` to help with i18n support (#881)
    * refactor: improve $message to work easier with i18n
    
    * chore: add tests
    
    * docs: add docs about i18n

---
+++ packages/test-project/src/components/I18nSimpleForm.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="SimpleForm">
+    <div class="lang-switcher">
+      Change Language to:
+      <a
+        href="#"
+        @click.prevent="$i18n.locale = 'bg'"
+      >BG</a> |
+      <a
+        href="#"
+        @click.prevent="$i18n.locale = 'en'"
+      >EN</a>
+    </div>
+    <div>
+      <label>name</label>
+      <input
+        v-model="name"
+        type="text"
+      >
+    </div>
+    <div>
+      <label>twitter</label>
+      <input
+        v-model="social.twitter"
+        type="text"
+      >
+    </div>
+    <div>
+      <label>github</label>
+      <input
+        v-model="social.github"
+        type="text"
+      >
+    </div>
+    <button @click="validate">
+      Validate
+    </button>
+    <button @click="v$.$touch">
+      $touch
+    </button>
+    <button @click="v$.$reset">
+      $reset
+    </button>
+    <div style="background: rgba(219, 53, 53, 0.62); color: #ff9090; padding: 10px 15px">
+      <p
+        v-for="(error, index) of v$.$errors"
+        :key="index"
+        style="padding: 0; margin: 5px 0"
+      >
+        {{ error.$message }}
+      </p>
+    </div>
+    <pre>{{ v$ }}</pre>
+  </div>
+</template>
+
+<script>
+import { ref, reactive, computed } from 'vue'
+import useVuelidate from '@vuelidate/core'
+import { required, helpers, minLength } from '@vuelidate/validators'
+import { i18n } from '../i18n'
+
+const { global: { t } } = i18n
+const { withAsync } = helpers
+
+const asyncValidator = withAsync((v) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(v === 'aaaa')
+    }, 2000)
+  })
+})
+
+const withI18nMessage = (validator) => helpers.withMessage((props) => t(`messages.${props.$validator}`, {
+  model: props.$model,
+  property: props.$property,
+  ...props.$params
+}), validator)
+
+export default {
+  name: 'I18nForm',
+  setup () {
+    const name = ref('given name')
+    const social = reactive({
+      github: 'hi',
+      twitter: 'hey'
+    })
+
+    let v$ = useVuelidate(
+      {
+        name: {
+          required: withI18nMessage(required),
+          asyncValidator: withI18nMessage(asyncValidator)
+        },
+        social: {
+          github: { minLength: withI18nMessage(minLength(computed(() => social.twitter.length))) },
+          twitter: { minLength: withI18nMessage(minLength(computed(() => name.value.length))) }
+        }
+      },
+      { name, social },
+      { $autoDirty: true }
+    )
+    return { name, v$, social }
+  },
+  methods: {
+    async validate () {
+      const result = await this.v$.$validate()
+      console.log('Result is', result)
+    }
+  }
+}
+</script>
+<style>
+.lang-switcher a {
+  color: white;
+}
+</style>
--- packages/test-project/src/routes.js
+++ packages/test-project/src/routes.js
@@ -2,11 +2,16 @@ import SimpleForm from './components/SimpleForm.vue'
 import NestedValidations from './components/NestedValidations.vue'
 import OldApiExample from './components/OldApiExample.vue'
 import ChainOfRefs from './components/ChainOfRefs.vue'
+import I18nSimpleForm from './components/I18nSimpleForm.vue'
 
 export const routes = [
   {
     path: '/',
     component: SimpleForm
+  },
+  {
+    path: '/i18n-simple',
+    component: I18nSimpleForm
   },
   {
     path: '/nested-validations',
