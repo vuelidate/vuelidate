2c1f83e96d072c8a4223a98f8ce93db31d392ec2
Ahmed Ghorab <aghorab@outlook.com>
2022-03-28 21:30:07 +0200

    fix(types): build issue with typescript undefined error (#1024)
    * Update index.d.ts
    
    enforce the ValidationArgs to have a value and force typescript not identify it as a undefined
    
    * add T State to depend on ValidationArgs
    
    Make ValidationArgs without parameters 
    Make sure that the state has every key in the validation to ensure correct state parameters
    
    * revert Validation Args type
    
    * revert ValidationArgs to default

--- packages/vuelidate/index.d.ts
+++ packages/vuelidate/index.d.ts
@@ -58,7 +58,7 @@ export type ValidationRule <T = any> = ValidationRuleWithParams<any, T> | Valida
 export type ValidationRuleCollection <T = any> = Record<string, ValidationRule<T>>;
 
 export type ValidationArgs<T = unknown> = {
-  [key in keyof T]?: ValidationArgs<T[key]> | ValidationRuleCollection<typeof T[key]> | ValidationRule<typeof T[key]>
+  [key in keyof T]: ValidationArgs<T[key]> | ValidationRuleCollection<typeof T[key]> | ValidationRule<typeof T[key]>
 }
 
 export interface RuleResultWithoutParams {
@@ -179,8 +179,8 @@ export interface GlobalConfig {
 
 export function useVuelidate(globalConfig?: GlobalConfig): Ref<Validation>;
 export function useVuelidate<
-  T extends Object,
-  Vargs extends ValidationArgs<T> = ValidationArgs<T>,
+  T extends {[key in keyof Vargs]: any},
+  Vargs extends ValidationArgs = ValidationArgs,
   EState extends ExtractState<Vargs> = ExtractState<Vargs>
 >(
   validationsArgs: Ref<Vargs> | Vargs,
