3512086a324d4e4bf800b8f7fb5f68ed88da76fe
Yuri Karapetyan <yk1711@gmail.com>
2022-07-06 15:14:48 +0300

    Update to Vue 2.7

--- packages/vuelidate/src/index.js
+++ packages/vuelidate/src/index.js
@@ -43,8 +43,9 @@ export function useVuelidate (validations, state, globalConfig = {}) {
   if (!$registerAs && instance) {
     // NOTE:
     // ._uid // Vue 2.x Composition-API plugin
+    // .proxy._uid // Vue 2.7
     // .uid // Vue 3.0
-    const uid = instance.uid || instance._uid
+    const uid = instance.uid || instance._uid || instance.proxy._uid
     $registerAs = `_vuelidate_${uid}`
   }
   const validationResults = ref({})
