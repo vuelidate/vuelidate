78c6940fb7974536cedb1d6cd473d1c14ab2ff28
Nikita Skazki <nskazki@gmail.com>
2025-04-28 19:52:44 +1000

    Lose the test project

--- packages/test-project/src/router.js
+++
@@ -1,7 +0,0 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { routes } from './routes.js'
-
-export const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
